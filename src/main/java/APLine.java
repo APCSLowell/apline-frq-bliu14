d218151fdaf496681769612c05ee7d2d78b9dd9b
bliu14 <143124039+bliu14@users.noreply.github.com>
2024-04-22 11:40:21 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,15 @@
 public class APLine
 {
   /* your code here */
-  
+  int a;
+  int b;
+  int c;
+  public APLine(int a, int b, int c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+  public double getSlope() {
+    
+  }
 }
